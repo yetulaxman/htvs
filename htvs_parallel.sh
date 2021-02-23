fe276af1060e56231bc4d9afdbd2c4e26acb10c4
Laxmana Yetukuri <yetukuri@puhti-login1.bullx>
2021-02-23 23:18:57 +0200

    initial tests

---
+++ htvs_parallel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20 # 40
+#SBATCH --time=00:10:10 # 40:00:00
+#SBATCH --mem=1G # 160G
+#SBATCH --account=project_2001659
+
+module load maestro parallel
+
+find data  -name '*.sdf' | \
+parallel -j $SLURM_CPUS_PER_TASK bash ${SLURM_SUBMIT_DIR}/wrapper.sh {}
+
+##parallel -j $SLURM_CPUS_PER_TASK ::: "ligprep -isd test1.sdf -osd test1.sd -LOCAL -NOJOBID"  "ligprep -isd test2.sdf -osd test\
+2.sd -LOCAL -NOJOBID"
+
---
+++ wrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# $1 is relative path to input file, 'dir-NNN/input-NNN'
+fpath=${1%/*}
+fname=${1#*/}
+
+# Full paths to parallel file system, '/scratch/...'
+input=$PWD/$1
+output=$PWD/$fpath
+mkdir -p $output
+
+# Change to local SSD disk
+rundir=$LOCAL_SCRATCH/$fpath
+mkdir -p $rundir && cd $rundir
+
+# Prepare input
+cp $input ./$fname
+
+# Place the actual app here instead of the dummy commands...
+
+file=$(echo $fname | cut -d'.' -f 1)
+ligprep -isd $fname -omae $file.mae  -LOCAL -NOJOBID
+
+# Move output from SSD to /scratch
+cp "$file".mae  $output
+
