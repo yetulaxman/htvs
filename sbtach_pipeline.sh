2846a7f6ccc61c69c33ee866d65f3079fb31b1c9
Laxmana Yetukuri <yetukuri@puhti-login2.bullx>
2021-02-26 09:06:44 +0200

    pipeline scripts

---
+++ sbtach_pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=2 # 40
+#SBATCH --time=00:10:10 # 40:00:00
+#SBATCH --mem=1G # 160G
+#SBATCH --account=project_2002389
+
+module load maestro parallel
+
+find $PWD/data_SMILES  -name '*.smi' | \
+parallel -j 10 bash ${SLURM_SUBMIT_DIR}/wrapper_ligprep_pipeline.sh {}
+#parallel -j $SLURM_CPUS_PER_TASK bash ${SLURM_SUBMIT_DIR}/wrapper_ligprep_pipeline.sh {}
---
+++ wrapper_ligprep_pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# $1 is absolute input file,/scratch/projects../... 
+fpath=${1%/*}
+fname=`basename $1`
+file=$(echo $fname | cut -d'.' -f 1) # file name without extension to create output dir with the name
+INPUT_SMILES="$fpath/$fname"
+PHASE_DB="$fpath/$file.phdb"
+output=$fpath/$file
+mkdir -p $output
+
+# Place the actual app commands here...
+cp template.inp $output &&  cd $output
+# NOTE: Do *NOT* add file extension as phase_database will add it automatically (.sdfgz)
+OUTPUT_SDF="$output"
+TMPINP=`mktemp phase_inputXXXX.inp`
+CPU_COUNT=2 # change this cautiously in production...
+
+sed -e "s:INPUT_SMILES:$INPUT_SMILES:" -e "s:PHASE_DB:$PHASE_DB:" template.inp >$TMPINP
+"${SCHRODINGER}/pipeline" -prog mydb $TMPINP -OVERWRITE -HOST localhost:$CPU_COUNT -NJOBS $CPU_COUNT -WAIT
+$SCHRODINGER/phase_database $PHASE_DB export -osd $OUTPUT_SDF -get 1 -limit 10000000 -WAIT
+
