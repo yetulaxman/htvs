0cf77396ed933bd8c1006c273647821d0ddeb4ad
Laxmana Yetukuri <yetukuri@puhti-login2.bullx>
2021-04-13 08:40:00 +0300

    gnu_parallell

---
+++ gnu_parallel/htvs_pipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=1 # 40
+#SBATCH --time=15:10:10 # 40:00:00
+#SBATCH --account=project_2001659
+
+module load maestro parallel
+
+find /scratch/project_2004075/yetukuri/results_1000k_splits  -name '*.smi' | \
+parallel -j 38 bash ${SLURM_SUBMIT_DIR}/wrapper_ligprep_pipeline.sh {} 
