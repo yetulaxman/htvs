737eb44b74c4d005022bdfa1a4cb473df85fa88e
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-04-13 10:50:21 +0300

    Update htvs_pipeline_helper.sh

--- nonwrapper_withsplits/htvs_pipeline_helper.sh
+++ nonwrapper_withsplits/htvs_pipeline_helper.sh
@@ -1,5 +1,5 @@
 module load maestro 
-rm script_file.sh
+rm script_file.sh   # or if [ -f script_file.sh ]; then   rm script_file.sh; fi
 touch script_file.sh
 for filename in /scratch/project_2004075/yetukuri/htvs_serial/data_SMILES2/*smi; do
     echo $filename
