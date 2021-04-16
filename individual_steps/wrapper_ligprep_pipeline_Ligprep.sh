0ebff020dbdba833b0471c7259543a2e946df583
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-04-16 13:22:47 +0300

    Update wrapper_ligprep_pipeline_Ligprep.sh

--- individual_steps/wrapper_ligprep_pipeline_Ligprep.sh
+++ individual_steps/wrapper_ligprep_pipeline_Ligprep.sh
@@ -11,5 +11,5 @@ PHASE_DB="$fpath/$file.phdb"
 output=$fpath/$file
 mkdir -p $output &&  cd $output
 
-$SCHRODINGER/ligprep -kp -retain  -lab -epik -W e,-ph,7.0,-pht,2.0,-ms,16 -i 2 -t 4 -s 4 -ismi "$INPUT_SMILES".smi -omae "$file".maegz -HOST localhost:10 -JOBNAME Legprep_test -LOCAL -WAIT
+$SCHRODINGER/ligprep -kp -retain  -lab -epik -W e,-ph,7.0,-pht,2.0,-ms,16 -i 2 -t 4 -s 4 -ismi "$INPUT_SMILES" -omae "$file".maegz -HOST localhost:10 -JOBNAME Legprep_test -LOCAL -WAIT
 
