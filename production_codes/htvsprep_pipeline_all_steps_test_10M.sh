a81e26db14b0d8920f013068d4fe10d58940a524
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-05-17 16:50:18 +0300

    Update htvsprep_pipeline_all_steps_test_10M.sh

--- production_codes/htvsprep_pipeline_all_steps_test_10M.sh
+++ production_codes/htvsprep_pipeline_all_steps_test_10M.sh
@@ -20,5 +20,5 @@ echo "test10M.maegz" >> test_phase_splice.list
 mkdir -p database
 
 $SCHRODINGER/ligprep -kp -retain  -lab -epik -W e,-ph,7.0,-pht,1.5,-ms,16 -i 2 -t 8 -s 4 -ismi "$PWD/data_SMILES/test10M.smi" -omae test10M.maegz -HOST localhost:127 -NSTRUCT 10000 -JOBNAME Legprep_test -LOCAL -WAIT
-$SCHRODINGER/phase_database ${SLURM_SUBMIT_DIR}/database/test10M.phdb splice test_phase_splice.list  -new -fmt int  -JOB test_phase_splice -HOST localhost:127 -TMPDIR $PWD -WAIT
+$SCHRODINGER/phase_database ${SLURM_SUBMIT_DIR}/database/test10M.phdb splice test_phase_splice.list  -new -fmt int  -JOB test_phase_splice -HOST localhost:127  -WAIT
 $SCHRODINGER/phase_database ${SLURM_SUBMIT_DIR}/database/test10M.phdb revise test_revise -confs auto -max 1 -sample rapid -HOST localhost:127 -WAIT
