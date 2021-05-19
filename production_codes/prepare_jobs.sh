a805f648daa21e6bafbcecde704664b4c31627db
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-05-19 22:40:20 +0300

    Update prepare_jobs.sh

--- production_codes/prepare_jobs.sh
+++ production_codes/prepare_jobs.sh
@@ -1,6 +1,7 @@
 split -l 10000000 -d -a 1  --additional-suffix=.smi Enamine_REAL_lead-like_randomized_00.smi  Enamine_REAL_file_1_part
 mkdir -p temp && mv Enamine_REAL_lead-like_randomized* temp
-rm directories.txt && touch directories.txt
+if [ -f directories.txt ]; then   rm directories.txt ; fi
+touch directories.txt
 for filename in *.smi; do
     dir=$(echo $filename | cut -d'.' -f 1)
     mkdir -p $dir && mkdir -p $dir/data_SMILES
