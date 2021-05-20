0d4e67177bc0ef6a5d45134fb943b1e17f3d0e24
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-05-20 22:29:51 +0300

    Update prepare_jobs.sh

--- production_codes/prepare_jobs.sh
+++ production_codes/prepare_jobs.sh
@@ -1,6 +1,6 @@
 # here is an example split with one DB file and follow the same for other files as well,
 split -l 10000000 -d -a 1  --additional-suffix=.smi Enamine_REAL_lead-like_randomized_00.smi  Enamine_REAL_file_1_part
-# make sure that original DB files are not present in this directory otherwise, jobs will be created for original DB file
+# make sure that original DB files are not present in this directory otherwise, jobs will be created for original DB file as well
 mkdir -p temp && mv Enamine_REAL_lead-like_randomized* temp
 if [ -f directories.txt ]; then   rm directories.txt ; fi
 touch directories.txt # add folder names to a file for submitting jobs later
@@ -12,6 +12,6 @@ for filename in *.smi; do
     cp  htvsprep_production_pipeline_Mahti.sh $dir/
     cd $dir
     echo $PWD    
-  #  bash htvsprep_production_pipeline_all_steps.sh  data_SMILE/$filename  # don't submit now use array job scripts instead.
+  #  bash htvsprep_production_pipeline_all_steps.sh  data_SMILE/$filename  # don't submit now, use array job scripts instead.
     cd -
 done
