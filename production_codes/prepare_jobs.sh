102bc0b08647df875c8b9d5a7fee70600f70909e
Laxman <48151266+yetulaxman@users.noreply.github.com>
2021-06-11 23:07:39 +0300

    Update prepare_jobs.sh

--- production_codes/prepare_jobs.sh
+++ production_codes/prepare_jobs.sh
@@ -1,5 +1,5 @@
 # here is an example split with one DB file and follow the same for other files as well,
-split -l 10000000 -d -a 1  --additional-suffix=.smi Enamine_REAL_lead-like_randomized_00.smi  Enamine_REAL_file_1_part
+split -l 10000000 -d -a 2  --additional-suffix=.smi Enamine_REAL_lead-like_randomized_00.smi  Enamine_REAL_file_1_part
 # make sure that original DB files are not present in this directory otherwise, jobs will be created for original DB file as well
 mkdir -p temp && mv Enamine_REAL_lead-like_randomized* temp
 if [ -f directories.txt ]; then   rm directories.txt ; fi
