1aafba37fcfdc1b5e291b6a400647d443c3221d7
Laxmana Yetukuri <yetukuri@puhti-login1.bullx>
2021-08-22 23:11:30 +0300

    small changes in documentation

--- htvs_pipeline.sh
+++ htvs_pipeline.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=1G # 160G
 #SBATCH --account=project_2001659
 
-module load maestro parallel
+module load maestro parallel  # load module
 
 find $PWD/data_SMILES  -name '*.smi' | \
 parallel -j 10 bash ${SLURM_SUBMIT_DIR}/wrapper_ligprep_pipeline.sh {}
