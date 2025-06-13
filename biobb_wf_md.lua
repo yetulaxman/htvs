a20206693f18ff2f0bb12aa84e19c634fee3a2f8
Laxman <48151266+yetulaxman@users.noreply.github.com>
2025-06-13 22:28:20 +0300

    Create biobb_wf_md.lua

---
+++ biobb_wf_md.lua
@@ -0,0 +1,8 @@
+-- Jupyter
+prepend_path("PATH", "/projappl/project_465001676/yetukuri/biobb_md/bin")
+
+setenv("_COURSE_BASE_NAME","biobb-wf")
+setenv("_COURSE_NOTEBOOK","biobb_wf_md_setup/biobb_wf_md_setup/notebooks/biobb_wf_md_setup.ipynb")
+setenv("_COURSE_GIT_REPO","https://github.com/bioexcel/biobb_wf_md_setup")
+setenv("_COURSE_GIT_REF","")
+setenv("_COURSE_NOTEBOOK_TYPE", "notebook")
